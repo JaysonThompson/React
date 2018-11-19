0a19bbaee1379d6d625aaeb2d881b026a248b5aa
Jayson Thompson <jaysonthompson922@gmail.com>
2018-11-18 21:01:02 -0800

    Adds new fish component and add to order functionality

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -15,6 +15,11 @@ class App extends React.Component {
     fishes[`fish${Date.now()}`] = fish;
     this.setState({ fishes });
   };
+  addToOrder = key => {
+    const order = { ...this.state.order };
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  };
   loadSampleFishes = () => {
     this.setState({ fishes });
   };
@@ -25,7 +30,12 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
